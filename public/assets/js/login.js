2fb9f346b55784808f56a5064959e57c43c3bffc
K1Pro <support@k1pro.net>
2023-09-01 13:12:33 +0200

    Refactored login

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -67,27 +67,19 @@ async function postUserData(
   url = srvrURL,
   username,
   password,
-  // data = {},
   errorMsg = 'Something went wrong'
 ) {
-  // Default options are marked with *
   const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    // mode: 'cors', // no-cors, *cors, same-origin
-    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: 'same-origin', // include, *same-origin, omit
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    // redirect: 'follow', // manual, *follow, error
-    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify({
       username: username,
       password: password,
-    }), // body data type must match "Content-Type" header
+    }),
   });
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response.json();
 }
 
 async function postEmailData(url = emailURL, accessToken) {
@@ -102,14 +94,3 @@ async function postEmailData(url = emailURL, accessToken) {
 }
 
 validationCookie();
-
-// postUserData(srvrURL, 'Bartosz', '7257Touhy!').then((data) => {
-//   console.log(data);
-//   if (data.data.access_token) {
-//     document.cookie = `_access_token=${data.data.access_token}`;
-//     console.log(getCookie('_access_token'));
-//   }
-// });
-
-//   let loggedInUser = localStorage.getItem('BundleContactList-LoginName');
-// console.log(localStorage.getItem('_access_token'));
